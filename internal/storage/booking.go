c29020642c723d8b44a491b0b00ff542714b7a13
oleksii.reshetnik <oleksii.reshetnik@pm.bet>
2022-07-29 13:28:02 +0200

    fix: fixed query

--- internal/storage/booking.go
+++ internal/storage/booking.go
@@ -9,7 +9,7 @@ import (
 const (
 	InsertBookingQuery = `INSERT INTO bookings (first_name, last_name, gender, birthday, launchpad_id, destination_id, launch_date) VALUES ($1, $2, $3, $4, $5, $6, $7) returning id;`
 	SelectBookingQuery = "SELECT * FROM bookings WHERE id = $1;"
-	DeleteBookingQuery = "DELETE * FROM bookings WHERE id = $1;"
+	DeleteBookingQuery = "DELETE FROM bookings WHERE id = $1;"
 )
 
 func (c *Client) CreateBooking(ctx context.Context, booking model.Booking) (string, error) {
