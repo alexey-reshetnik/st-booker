65b32f9bfa96660df00c292084d52cbd7dad6f1c
oleksii.reshetnik <oleksii.reshetnik@pm.bet>
2022-07-29 08:03:24 +0200

    feat: added methods to storage

--- internal/storage/booking.go
+++ internal/storage/booking.go
@@ -6,14 +6,36 @@ import (
 	"st-booker/internal/model"
 )
 
+const (
+	InsertBookingQuery = `INSERT INTO bookings (first_name, last_name, gender, birthday, launchpad_id, destination_id, launch_date) VALUES ($1, $2, $3, $4, $5, $6, $7) returning id;`
+	SelectBookingQuery = "SELECT * FROM bookings WHERE id = $1;"
+	DeleteBookingQuery = "DELETE * FROM bookings WHERE id = $1;"
+)
+
 func (c *Client) CreateBooking(ctx context.Context, booking model.Booking) (string, error) {
-	return "", nil
+	var id string
+
+	row := c.conn.QueryRow(InsertBookingQuery, booking)
+	if err := row.Scan(&id); err != nil {
+		return "", err
+	}
+
+	return id, nil
 }
 
 func (c *Client) GetBookings(ctx context.Context, limit, offset int) ([]model.Booking, error) {
-	return []model.Booking{}, nil
+	var booking []model.Booking
+
+	err := c.conn.Get(&booking, SelectBookingQuery, booking)
+
+	return booking, err
 }
 
 func (c *Client) DeleteBooking(ctx context.Context, id string) error {
-	return nil
+	_, err := c.conn.ExecContext(ctx, DeleteBookingQuery, id)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
--- internal/storage/client.go
+++ internal/storage/client.go
@@ -1,8 +1,25 @@
 package storage
 
+import (
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+)
+
 type Client struct {
+	conn *sqlx.DB
 }
 
-func NewClient() *Client {
-	return &Client{}
+func NewClient(db *sql.DB) (*Client, error) {
+	sqlxDB := sqlx.NewDb(db, "postgres")
+
+	errPing := db.Ping()
+	if errPing != nil {
+		return nil, errPing
+	}
+
+	return &Client{
+			conn: sqlxDB,
+		},
+		nil
 }
