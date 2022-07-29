d807f68ecba8fdc7e668eb05178c4530a1a6db42
oleksii.reshetnik <oleksii.reshetnik@pm.bet>
2022-07-29 08:04:53 +0200

    feat: updated main file with config & migrations

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
+	"st-booker/internal/config"
 	"st-booker/internal/server"
 	"st-booker/internal/service"
 	"st-booker/internal/storage"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
 	"go.uber.org/zap"
 )
 
@@ -17,7 +22,22 @@ func main() {
 		log.Fatalln("failed to init logger")
 	}
 
-	st := storage.NewClient()
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		lg.Error(err.Error())
+	}
+
+	db, err := sql.Open("postgres", connStr(&cfg))
+	if err != nil {
+		lg.Error(err.Error())
+	}
+
+	err = migrateUp(db)
+	if err != nil {
+		lg.Error(err.Error())
+	}
+
+	st, err := storage.NewClient(db)
 
 	srv := service.NewBooking(st, lg)
 
@@ -28,4 +48,30 @@ func main() {
 	if err := r.Run(":8080"); err != nil { //TODO: move port to config
 		lg.Error(err.Error())
 	}
+}
+
+func migrateUp(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://migrations", "postgres", driver)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func connStr(c *config.Config) string {
+	pConfig := c.DB
+
+	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s ",
+		pConfig.Host, pConfig.Port, pConfig.Username, pConfig.DBName, pConfig.Password, pConfig.SSLMode)
 }
